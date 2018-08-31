33576222ffd609624479e503583bc741b7e4013c
adanyao <adanyao>
2018-08-31 20:27:20 +0800

    first commit java

---
+++ day3/ex2.java
@@ -0,0 +1,36 @@
+class ex2
+{
+	public static void main(String args[])
+	{
+		
+int x=10;
+		int y;
+		
+		//x++;
+		//++x;
+		//y=x;
+		y=x++;//y=x;x=x+1;
+		//y=++x;//x=x+1;y=x;
+		
+		System.out.println("x="+x);
+		System.out.println("y="+y);
+
+
+
+   /*
+		int x=10;
+
+		//x=x+1;
+		//x+=1
+		//x++;
+		++x;
+		System.out.println("x="+x);
+        //x=x-1
+        //x-=1;
+        //x--;
+        --x;
+
+		System.out.println("x="+x);
+		*/
+	}
+}
---
+++ day3/ex3.java
@@ -0,0 +1,26 @@
+class ex3
+{
+	public static void main(String args[])
+	{
+
+		int chi=65;
+
+		int eng=78;
+
+		System.out.println("chi>60 && eng>=75 ---->"+!(chi>=60 && eng>=75)); // +! 反轉
+             System.out.println("chi>=60 || eng>=75-->"+(chi>=60 || eng>=75));
+          System.out.println("chi>60 ^ eng>=75 ---->"+(!(chi>=60) ^ eng>=75));  //^ XOR  (!) 反轉
+
+/*
+		int chi=65;
+
+		System.out.println("chi>60="+(chi>60));
+		System.out.println("chi<60="+(chi<60));
+		System.out.println("chi>=60="+(chi>=60));
+        System.out.println("chi<=60="+(chi<=60));
+		System.out.println("chi==60="+(chi==60));
+		System.out.println("chi!=60="+(chi!=60));
+		*/
+
+}
+}
