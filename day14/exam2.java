8531775cbeb94e6d32e92826eaa5d6c679daf4bf
adanyao <adanyao>
2018-09-17 17:01:49 +0800

    modify gitignore

--- day14/exam2.java
+++ day14/exam2.java
@@ -1,10 +1,19 @@
 import java.io.File;
+import java.io.IOException;
 class exam2
 {
 	public static void main(String args[])
 
 	{
         File f1= new File("/home/adan/a.txt");
-        f1.createNew file();
+        try
+        {
+        f1.createNewFile();
+        }
+
+        catch(IOException e)
+        {
+        	System.out.println("無新增權限");
+        }
 	}
 }
